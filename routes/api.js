9f4c3e4b8154d2a5c11c2fc8536d82a9db65345c
Iris Valle <iris.valle.design@gmail.com>
2021-08-03 15:04:14 -0400

    adding api routes

---
+++ routes/api.js
@@ -0,0 +1,52 @@
+const router = require("express").Router();
+const Workout = require("../models/workout.js");
+
+router.post("/api/workouts", (req, res) => {
+  Workout.create({})
+  .then(dbWorkout => {
+    res.json(dbWorkout);
+  })
+  .catch(err => {
+    res.status(400).json(err);
+  });
+});
+
+router.put("/api/workouts/:id", (req, res) => {
+  console.log(req.body);
+  Workout.findByIdAndUpdate({ _id: req.params.id }, { $push: { exercises: req.body } })
+  .then(dbWorkout => {
+    res.json(dbWorkout);
+  })
+  .catch(err => {
+    res.status(400).json(err);
+  });
+});
+
+router.get("/api/workouts", (req, res) => {
+  Workout.aggregate([
+    { $addFields: {
+      totalDuration: {
+        $sum:  "$exercises.duration" 
+      }
+    } },
+  ])
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+router.get("/api/workouts/range", (req, res) => {
+  Workout.find({})
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+
+module.exports = router;
