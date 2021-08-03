5ddd16e67c849020b0a64dabc362109a905a7f84
Iris Valle <iris.valle.design@gmail.com>
2021-08-03 15:03:33 -0400

    updating workout model

--- models/workout.js
+++ models/workout.js
@@ -7,32 +7,36 @@ const workoutSchema = new Schema({
         type: Date,
         default: Date.now
       },
-      type: {
-        type: String,
-        trim: true,
-        required: "Enter type of exercise"
-      },
-      name: {
-        type: String,
-        trim: true,
-        required: "Enter a name for the exercise"
-      },
-      duration: {
-        type: Number,
-        required: "Enter an amount"
-      },
-      weight: {
-        type: Number,
-        required: "Enter an amount"
-      },
-      reps: {
-        type: Number,
-        required: "Enter an amount"
-      },
-      sets: {
-        type: Number,
-        required: "Enter an amount"
-      }
+      exercises: [
+        {
+          type: {
+            type: String,
+            trim: true,
+            required: "Enter type of exercise"
+          },
+          name: {
+            type: String,
+            trim: true,
+            required: "Enter a name for the exercise"
+          },
+          duration: {
+            type: Number,
+            required: "Enter an amount"
+          },
+          weight: {
+            type: Number
+          },
+          reps: {
+            type: Number
+          },
+          sets: {
+            type: Number
+          },
+          distance: {
+            type: Number
+          }
+        }
+      ]
 });
 
 const Workout = mongoose.model("Workout", workoutSchema);
